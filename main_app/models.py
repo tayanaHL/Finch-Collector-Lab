b6ba6948a55c9c29864445977b5ea4d84ed8e3df
Tayana <tayana1498@gmail.com>
2023-03-10 14:41:51 -0700

    got it working

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
