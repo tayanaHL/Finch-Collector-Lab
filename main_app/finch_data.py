70658834d52baf3bc0d5e61076e81d3c0307b721
Tayana <tayana1498@gmail.com>
2023-03-11 21:31:13 -0700

    update

--- main_app/finch_data.py
+++ main_app/finch_data.py
@@ -1,9 +1,10 @@
-
-
 finches = [
     {'species': 'Galapagos Finch', 'beak_depth': 10, 'beak_width': 5, 'island': 'Galapagos'},
     {'species': 'Cactus Finch', 'beak_depth': 8, 'beak_width': 4, 'island': 'Galapagos'},
     {'species': 'Ground Finch', 'beak_depth': 7, 'beak_width': 3, 'island': 'Galapagos'},
     {'species': 'Tree Finch', 'beak_depth': 9, 'beak_width': 6, 'island': 'Galapagos'},
-    {'species': 'Warbler Finch', 'beak_depth': 6, 'beak_width': 2, 'island': 'Galapagos'}
+    {'species': 'Warbler Finch', 'beak_depth': 6, 'beak_width': 2, 'island': 'Galapagos'},
+    {"species": "Geospiza fortis", "beak_depth": 8.6, "beak_width": 6.2, "island": "Daphne Major"},
+    {"species": "Geospiza magnirostris", "beak_depth": 10.4, "beak_width": 7.3, "island": "Santa Cruz"},
+    {"species": "Geospiza parvula", "beak_depth": 9.5, "beak_width": 6.5, "island": "Santa Cruz"}
 ]
